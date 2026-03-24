500ebebfccccafe64d84c667534d11ebf941fa19
Venture-Hacker <diljith8129@outlook.com>
2026-03-24 15:45:01 +0530

    vercel final commit

--- flynext/settings.py
+++ flynext/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +49,8 @@ INSTALLED_APPS = [
 ]
 
 SOCIALACCOUNT_LOGIN_ON_GET=True
+
+
 
 
 # Accessing details from google
@@ -99,12 +103,12 @@ WSGI_APPLICATION = 'flynext.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
