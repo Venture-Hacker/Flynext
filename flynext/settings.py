d283f28be9fd27301ed6097159ace0da1e94e8aa
Venture-Hacker <diljith8129@outlook.com>
2026-03-25 01:11:21 +0530

    final commit for railway

--- flynext/settings.py
+++ flynext/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kgcmb1!ld8n77lt=07b09!dake&4)do%7ad3gksbwij%8im76k'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
